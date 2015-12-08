bf96c43670d8e82257586e9c19c5be9ded0c40b9
Asko Kauppi <asko@bmdesign.fi>
2015-12-08 07:00:54 +0200

    debugging how touch events work

--- demo/demo4.js
+++ demo/demo4.js
@@ -6,33 +6,59 @@
 * Follow each finger touch with a circle of different colour, and tie them together in the order of the touches.
 *
 * Allow any touch to be removed while following.
+*
+* Credit:
+*   http://tomicloud.com/2012/03/multi-touch-demo
 */
 
 (function() {
   var R=60;
 
-  var svg = SVG("cradle");   // fills the cradle fully
+  var svg = SVG("cradle");
 
-  /*
+  //var el = svg.circle(20).center(0,0);
+
+  var genHandler = function (name) {
+    return function (ev) {
+      //console.log(name);
+
+      for( var i=0; i< ev.changedTouches.length; i++ ) {
+        var touch = ev.changedTouches[i];
+        var x = touch.clientX;
+        var y = touch.clientY;
+        console.log(name + " "+ touch.identifier + ": "+ x +" "+ y);
+      }
+    }
+  }
+
+  svg.node.addEventListener("touchstart", genHandler("touchstart"), false);
+  svg.node.addEventListener("touchend", genHandler("touchend"), false);
+  svg.node.addEventListener("touchcancel", genHandler("touchcancel"), false);
+  svg.node.addEventListener("touchmove", genHandler("touchmove"), false);
+
+  /*** needs editing
+  /_*
   * Helper to handle multitouch events
   *
   * i:  1..n (level of recursion) = Nth touch
   *
   * obs: observable of (observable of {x:Int,y:Int}, observable like 'obs' (recursively))
-  */
+  *_/
   var handleMultiTouch = function (i, obs) {
 
     obs.subscribe( function (obs1, obs2) {
-      // tbd. create a circle for the touch ('obs1' will move it around)
+      // create a circle for the touch ('obs1' will move it around)
+      //
+      var circle = svg.circle(0,0).attr("level",i);
 
       obs1.subscribe( function (o) {   // {x:Int,y:Int}
           console.log( "Moving touch: "+i );
           console.log(o);
-          // tbd. move the particular circle
+          circle.move(o.x,o.y);
         },
         function () {   // drag ended
           console.log( "Out of touch: "+i );
-          // tbd. remove the circle
+          circle.release();
         }
       )
 
@@ -41,5 +67,6 @@
   };
 
   handleMultiTouch( 1, svg.rx_multitouch() )
+  ***/
 
 })();
