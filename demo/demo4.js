856b3e45aa2710b2b262a65d841423135828e65e
Asko Kauppi <akauppi@gmail.com>
2015-12-20 18:18:07 +0100

    from DOR

--- demo/demo4.js
+++ demo/demo4.js
@@ -10,6 +10,8 @@
 * Credit:
 *   http://tomicloud.com/2012/03/multi-touch-demo
 */
+/*global a,b,c */
+"use strict";
 
 (function() {
   var R=60;
@@ -17,48 +19,51 @@
 
   var svg = SVG("cradle");
 
-  var genHandler = function (name) {
-    return function (ev) {
+  var circle = [];
 
-      console.log(ev.changedTouches);
-
-      // Note: 'ev.changedTouches' does not work like an array, i.e. no '.map' and such. AKa131215
-      //
-      var s="";
-
-      for( var i=0; i< ev.changedTouches.length; i++ ) {
-        var touch = ev.changedTouches[i];
-        var x = touch.clientX;
-        var y = touch.clientY;
-        s += touch.identifier + ": "+ x +" "+ y + " ";
-      }
-
-      console.log(name + " "+ s);
-
-      /***
-      for( var i=0; i< ev.changedTouches.length; i++ ) {
-        var touch = ev.changedTouches[i];
-        var x = touch.clientX;
-        var y = touch.clientY;
-        console.log(name + " "+ touch.identifier + ": "+ x +" "+ y);
-      }
-      ***/
-    }
+  for( var i=0; i<N; i++ ) {
+    circle[i] = svg.circle(R).class("touch"+i).hide();
   }
 
-  svg.node.addEventListener("touchstart", genHandler("touchstart"), false);
-  svg.node.addEventListener("touchend", genHandler("touchend"), false);
-  svg.node.addEventListener("touchcancel", genHandler("touchcancel"), false);
-  svg.node.addEventListener("touchmove", genHandler("touchmove"), false);
+  // Object for first touch is always visible
+  //
+  circle[0].center(100,100).show();
 
-  /*** PENDING
+  // Catch the first touch
+  //
+  var outerObs0 = circle[0].rx_touch(0);   // observable of observables of {x: Int, y: Int}
+
+  outerObs0.subscribe( function (dragObs0) {
+
+    // track the other N-1 touches, while the first one is held
+    //
+    var circles = [];
+    var obs = [];
+
+    for( var i=1; i<N; i++ ) {
+      obs[i-1] = window.rx_touch(i);
+
+      obs[i-1].subscribe( function (dragObs) {
+        circle[i].center(o.x, o.y).show();
+      });
+    }
+
+    dragObs0.subscribe( function (o) {
+      circle[0].center(o.x, o.y);
+    });
+
+  }, function () {    // end of drag
+    for( var i=1; i<N; i++ ) {
+      circles[i].hide();
+      obs[i-1].cancel();
+    }
+  })
+
   // Start getting events for N fingers
   //
-  var arr = svg.rx_touch(N);    // [(observable of obseravables of ({x: Int, y: Int})), ...]
+  var arr = svg.rx_touch(N);    // [observable of obseravables of ({x: Int, y: Int}), ...]
 
   arr.forEach( function (obsOuter,i) {
-
-    var circle = svg.circle(R).class("touch"+i).hide();
 
     obsOuter.subscribe( function (obsDrag) {
       dragObs.subscribe( function (o) {
@@ -70,6 +75,37 @@
       circle.hide();    // end of touch
     } );
   });
-  ***/
 
 })();
+
+
+/***
+  // debugging
+  //
+  if (false) {
+    var genHandler = function (name) {
+      return function (ev) {
+        console.log(ev.changedTouches);
+
+        // Note: 'ev.changedTouches' does not work like an array, i.e. no '.map' and such. AKa131215
+        //
+        var s="";
+
+        for( var i=0; i< ev.changedTouches.length; i++ ) {
+          var touch = ev.changedTouches[i];
+          var x = touch.clientX;
+          var y = touch.clientY;
+          s += touch.identifier + ": "+ x +" "+ y + " ";
+        }
+
+        console.log(name + " "+ s);
+      }
+    }
+
+    svg.node.addEventListener("touchstart", genHandler("touchstart"), false);
+    svg.node.addEventListener("touchend", genHandler("touchend"), false);
+    svg.node.addEventListener("touchcancel", genHandler("touchcancel"), false);
+    svg.node.addEventListener("touchmove", genHandler("touchmove"), false);
+  }
+
+***/
--- src/svg.rx.js
+++ src/svg.rx.js
@@ -1,17 +1,107 @@
 /*
 * svg.rx.js
 */
-
-// assert( SVG.Element.prototype );
+/*global a,b,c */
+"use strict";
 
 // Note: The event handling code is based on 'svg.draggable.js' -> https://github.com/wout/svg.draggable.js
 //      but we only enable stuff that we actually test (manually). I.e. 'SVG.Nested', 'SVG.Use', 'SVG.Text' support
 //      remains disabled until we need it, and there are demos that exercise those things.
 
-/*
-*/
 (function() {
 
+  // Helper function to give us an outer stream of drags. Either coming from desktop, or touch.
+  //
+  // All parameters are 'observable of events', where the event may either be coming from mouse of (particular touch id's)
+  // touch events.
+  //
+  // mouse:
+  //    ... show event structure here ...
+  //
+  // touch:
+  //    ... touch event structure here ...
+  //
+  var outerObs = function (el, startObs, moveObs, endObs) {    // (SVG.Element, observable of mouseEvent or Touch, ...) -> observable of {x:Int, y:Int}
+
+    // Note: keep helper functions within the '.select()' function, since things like the positioning of the element,
+    //      its transformation etc. may change during the lifespan of the observable.
+
+    return startObs.
+      select( function (oStart) {
+        //console.log( "Outer observable started" );
+
+        // Transform from screen to user coordinates. Take care of pointer and touch events having different
+        // inner structures.
+        //
+        var transformP = (function () {    // scope
+
+          // Note: local values are hidden in this scope.
+          //
+          var parent = /*self.parent(SVG.Nested) ||*/ self.parent(SVG.Doc);
+
+          // tbd. Can we do these within 'svg.js', without using the '.node' (i.e. dropping to plain SVG APIs)?
+
+          var p = parent.node.createSVGPoint();     // point buffer (avoid reallocation per each coordinate change)
+          var m = self.node.getScreenCTM().inverse();
+
+          return function (o /*, offset*/) {   // (mouseEvent or Touch) -> point
+            p.x = o.pageX;  // - (offset || 0)
+            p.y = o.pageY;
+
+            return p.matrixTransform(m);
+          }
+        })();
+
+        /** DISABLED text element support not needed, yet
+        var anchorOffset;
+
+        // fix text-anchor in text-element (svg.draggable.js #37)
+        if (self instanceof SVG.Text) {
+          anchorOffset = self.node.getComputedTextLength();
+
+          switch (self.attr('text-anchor')) {
+            case 'middle':
+              anchorOffset /= 2;
+              break;
+            case 'start':
+              anchorOffset = 0;
+              break;
+          }
+        }
+        **/
+
+        var p0 = transformP(oStart /*, anchorOffset*/);
+
+        var x_offset = p0.x - el.x(),
+            y_offset = p0.y - el.y();
+
+        var endSingleObs = endObs.take(1);    // tbd. is there any benefit of doing this '.take(1)'? We're using '.takeUntil()' below.
+
+        // tbd. How to optimize so that only the last event would ever be shipped, if multiple have gathered, i.e.
+        //      we only need the last coordinates. AKa071015
+        //
+        // Note: some events actually come with the same x,y values (at least on Safari OS X) - removed by the
+        //      '.distinctUntilChanged()'.
+        //
+        var innerObs = moveObs.select( function (o) {
+          //console.log( evMove );
+
+          var p = transformP(o);
+
+          return {
+            x: p.x - x_offset,
+            y: p.y - y_offset
+          };
+        } )
+          .distinctUntilChanged()
+          .takeUntil( endSingleObs );
+
+        return innerObs;
+      } );
+    };  // function outerObs
+
+
+  /*** DISABLED
   SVG.extend( SVG.Element, {
 
     // Create an rxJS observable for any dragging events
@@ -31,129 +121,6 @@
 
       var self = this;    // to be used within further inner functions
 
-      // Helper function to give us an outer stream of drags. Either coming from desktop, or touch.
-      //
-      var outerObs = function (keys) {    // ({start:string, move:string, end:string}) -> observable of {x:Int, y:Int}
-
-        return Rx.Observable.fromEvent(self.node, keys.start)
-          .select( function (evStart) {
-            //console.log( "Outer observable started" );
-
-            // Transform from screen to user coordinates. Take care of pointer and touch events having different
-            // inner structures.
-            //
-            var transformP = (function () {    // scope
-
-              // Note: local values are hidden in this scope.
-              //
-              var parent = /*self.parent(SVG.Nested) ||*/ self.parent(SVG.Doc);
-
-              // tbd. Can we do these within 'svg.js', without using the '.node' (i.e. dropping to plain SVG APIs)?
-
-              var p = parent.node.createSVGPoint();     // point buffer (avoid reallocation per each coordinate change)
-              var m = self.node.getScreenCTM().inverse();
-
-              return function (ev /*, offset*/) {   // (mouse or touch event) -> point
-                var o = ev.changedTouches ? ev.changedTouches[0] : ev;
-                p.x = o.pageX;  // - (offset || 0)
-                p.y = o.pageY;
-
-                return p.matrixTransform(m);
-              }
-            })();
-
-            /** DISABLED text element support not needed, yet
-            var anchorOffset;
-
-            // fix text-anchor in text-element (svg.draggable.js #37)
-            if (self instanceof SVG.Text) {
-              anchorOffset = self.node.getComputedTextLength();
-
-              switch (self.attr('text-anchor')) {
-                case 'middle':
-                  anchorOffset /= 2;
-                  break;
-                case 'start':
-                  anchorOffset = 0;
-                  break;
-              }
-            }
-            **/
-
-            var p0 = transformP(evStart /*, anchorOffset*/);
-
-            var x_offset = p0.x - self.x(),
-                y_offset = p0.y - self.y();
-
-            // prevent browser drag behavior
-            //
-            evStart.preventDefault();
-
-            // prevent propagation to a parent that might also have dragging enabled (tbd. this probably also takes
-            // care of '.preventDefault()').
-            //
-            evStart.stopPropagation();
-
-            var obsMove = Rx.Observable.fromEvent(window, keys.move);
-
-            var obsUpSingle = Rx.Observable.fromEvent(window, keys.end)
-                                .take(1);    // tbd. is there any benefit of doing this '.take(1)'? We're using '.takeUntil()' below.
-
-            // tbd. How to optimize so that only the last event would ever be shipped, if multiple have gathered, i.e.
-            //      we only need the last coordinates. AKa071015
-            //
-            // Note: some events actually come with the same x,y values (at least on Safari OS X) - removed by the
-            //      '.distinctUntilChanged()'.
-            //
-            var obsInner = obsMove.select( function (evMove) {
-              //console.log( evMove );
-
-              var p = transformP(evMove);
-
-              return {
-                x: p.x - x_offset,
-                y: p.y - y_offset
-              };
-            } )
-              .distinctUntilChanged()
-              .takeUntil( obsUpSingle );
-
-            return obsInner;
-          } );
-        };  // function outerObs
-
-      /*** DISABLED - no 'root' in Safari. AKa251015
-      * Decide which events we observe.
-      *
-      * For any platform, it should be enough to just observe one kind of events, right? This part may need revising, e.g.
-      *
-      * - role of 'root.PointerEvent' - what is it, how is it connected with the normal mouse events?
-      *
-      var keys = null;
-
-      if (root.TouchEvent) {
-        keys = {
-          start: 'touchstart',
-          move: 'touchmove',
-          end: 'touchend'
-        };
-      } else if (root.PointerEvent) {   // TBD: not tested!!! (which browsers would have this, IE only?) AKa251015
-        keys = {
-          start: 'pointerstart',
-          move: 'pointermove',
-          end: 'pointerhend'
-        };
-      } else {        // traditional mouse fallback
-        keys = {
-          start: 'mousedown',
-          move: 'mousemove',
-          end: 'mouseup'
-        };
-      }
-
-      return outerObs(keys);
-      ***/
-
       // Merge the two approaches. Only one inner drag active at any one time (desktop or touch)
       //
       // Note: the caller should dispose of this (and we need to check if we need to dispose some).
@@ -165,60 +132,129 @@
     }
 
   });
+  ***/
 
-  /*
-  * The main document gets a method to track multiple touches within it.
-  *
-  * Note: We could enable this for 'SVG.Nested' as well, if we one day decide to support them.
-  */
+
   SVG.extend( SVG.Element, {
 
+    //---
+    // Create an RxJS observable for touch events of a certain touchId.
+    //
     // Returns:
-    //  observable of [<observable from rx_draggable>, <observable from rx_multitouch>]
+    //  observable of observables of { x: Int, y: Int }
     //
-    // This is a bit bizarre - but dead simple at the same time!
+    // 'n' is the touch index (0-based) to follow. Maximum number of touches that can be tracked depends on hardware.
     //
-    // An observable is returned that will get a message when a touch (first touch) within the given SVG element happens.
-    //
-    //  - the first parameter of it is a "draggable" observable that will follow such touch's moves, until it is cancelled
-    //    (roamed past the SVG boundaries) or ended (touch lifted).
-    //
-    //    This observable behaves precisely like those given by '.rx_draggable'. If the (first) touch happens again, later,
-    //    the draggable observable provides a new dragging experience, and so one.
-    //
-    //  - the second parameter is a recursive observable, in the sense that it's precisely like the one returned
-    //    by '.rx_multitouch', but for the next level (2nd) concurrent touch. The second touch (and thereafter) can happen
-    //    anywhere on the SVG document, not tied to the particular element. The limit of how many simultaneous touches
-    //    can be tracked comes from the hardware.
-    //
-    // Special cases:
-    //
-    //    If the last touch is ended, a new touch will be placed onto that level's draggable (i.e. a new level is not
-    //    created); this is trivial.
-    //
-    //    But what if a touch in the "middle" is lifted?
-    //
-    //    In this case, the remaining touches (let's say 1 and 3) continue their drags as if nothing had occured. Only
-    //    the touch 2 ended.
-    //
-    //    If in this case, a new touch is initiated, is it touch 2 or 4?
-    //
-    //    This is really a matter of specification, and what is deemed useful by the application code. Here, we make it
-    //    touch 4, i.e. we allow gaps to be formed in the touches (nb. by tapping two fingers off one after each other,
-    //    you'd get a multitouch where only touches n-1 and n are active, raising by each tap).
-    //
-    rx_multitouch: function (n) {   // () -> [observable of observables of { x: Int, y: Int }, ...]
+    rx_touch: function (n) {   // (n: Int) -> observable of observables of { x: Int, y: Int }
 
-      // implement when knowing touch events really, really good!!!
-      //...
-      var doc = this.doc;    // to be used within further inner functions
+      var self = this;    // to be used within further inner functions
 
-      var obs = this.rx_draggable();    // observable of observables of { x: Int, y: Int }
+      // tbd. Could make those filter the data so that it's uniform with pointer events
+      //
 
-      return obs.select( function (obsDrag) {
-        return [obsDrag, doc.rx_multitouch(n+1)];
-      } );
+      // Return an observable for certain touch events for the element. Also remember the observable for later calls
+      // (only one is ever created, for a certain element and 'evName').
+      //
+      var cache = function (evName) {   // (String) -> observable of touchEvent
+        var key = "svgrxjs."+evName;
+
+        var obs = self.remember(obs);
+        if (!obs) {
+          obs = Rx.Observable.fromEvent(self, evName);
+          self.remember(key,obs);
+        }
+        return obs;
+      }
+
+      var startAllObs = cache( "touchstart" );
+      var moveAllObs = cache( "touchmove" );
+      var cancelAllObs = cache( "touchcancel" );
+      var endAllObs = cache( "touchend" );
+
+      // Derive the observable for just the given touch
+      //
+      var f = function (ev) {
+
+        // tbd. Wasn't able to use normal array methods with 'ev.changedTouches'. We'd want '.find()'. AKa161215
+        //
+        for (var i=0; i<ev.changedTouches.length; i++) {
+          var touch = ev.changedTouches[i];
+          if (touch.identifier == n) {
+            return touch;
+          }
+        }
+      }
+
+      var startObs = startAllObs.select(f);
+      var moveObs = moveAllObs.select(f);
+      var cancelObs = cancelAllObs.select(f);
+      var endObs = endAllObs.select(f);
+
+      var cancelOrEndObs = Rx.Observable.merge( endObs, cancelObs );
+
+      return outerObs( self, startObs, moveObs, cancelOrEndObs );
+    }
+
+    //---
+    // Pointer tracking for the element
+    //
+    rx_desktop: function () {
+      var self = this;
+
+      var startObs =  Rx.Observable.fromEvent(self.node, "mousestart");
+      var moveObs =   Rx.Observable.fromEvent(window, "mousemove");
+      var endObs =    Rx.Observable.fromEvent(window, "mouseup");
+
+      // Prevent browser drag behaviour. Note: wonder if we should do the same also for touch #1 evens. tbd AKa171215
+      //
+      startObs.select( function (ev) {
+        // prevent browser drag behavior
+        //
+        ev.preventDefault();
+
+        // prevent propagation to a parent that might also have dragging enabled (tbd. this probably also takes
+        // care of '.preventDefault()').
+        //
+        ev.stopPropagation();
+      });
+
+      // tbd. check if we should just have this, or merge with 'rx_touch(0)' stream ('Rx.Observable.merge').
+      //
+      return outerObs( self, startObs, moveObs, endObs );
     }
   });
 
 })();
+
+
+  /*** DISABLED - no 'root' in Safari. AKa251015
+  * Decide which events we observe.
+  *
+  * For any platform, it should be enough to just observe one kind of events, right? This part may need revising, e.g.
+  *
+  * - role of 'root.PointerEvent' - what is it, how is it connected with the normal mouse events?
+  *
+  var keys = null;
+
+  if (root.TouchEvent) {
+    keys = {
+      start: 'touchstart',
+      move: 'touchmove',
+      end: 'touchend'
+    };
+  } else if (root.PointerEvent) {   // TBD: not tested!!! (which browsers would have this, IE only?) AKa251015
+    keys = {
+      start: 'pointerstart',
+      move: 'pointermove',
+      end: 'pointerhend'
+    };
+  } else {        // traditional mouse fallback
+    keys = {
+      start: 'mousedown',
+      move: 'mousemove',
+      end: 'mouseup'
+    };
+  }
+
+  return outerObs(keys);
+  ***/
