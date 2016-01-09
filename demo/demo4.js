f4d367dea5c22a0d89d45d9091ed488ccba2bc13
Asko Kauppi <asko@bmdesign.fi>
2016-01-09 19:32:18 +0200

    hmm... thinking about it and asking on SO

--- demo/demo4.js
+++ demo/demo4.js
@@ -20,21 +20,52 @@
 
   var c= -1;   // next CSS color to use
 
+  // Note: Within the multi-touch demo, it is obvious we should have some back pressure handling in place. Moving
+  //      the SVG circles through all the received coordinates is unnecessary - the circles drag behind the finder.
+  //      However, how to implement this currently (rx-lite 4.0.7; 9-Jan-16) is by no means clear.
+  //
+  //      Tried all the suggested approaches; none of them work.
+  //
+  //        - '.throttleFirst' simply didn't seem to be there as 'dragObs's method
+  //
+  //      Using '.sample' with a trigger managed here on the application side is probably the best approach, but didn't
+  //      get even that to work (see below).
+  //
+  //      RxJS documentation says about these things:
+  //
+  //        "In future versions of RxJS, the idea of the controlled observable will be baked into the subscription
+  //        itself which then allows the backpressure to be an essential part of the contract or requesting n number of items."
+  //
+  //      Ref.
+  //        RxJS Getting Started > Backpressure -> https://github.com/Reactive-Extensions/RxJS/blob/master/doc/gettingstarted/backpressure.md
+  //
   svg.rx_draggable().subscribe( function (dragObs) {
     c = (c+1) % COLORS;
 
     var circle = svg.circle(R).addClass("n"+c).hide();
 
-    dragObs.subscribe(
-      function (o) {
-        //console.log( "Dragging: "+ o.x + " "+ o.y );
-        circle.center(o.x, o.y).show();
-      },
-      null,   // error handling
-      function () {  // end of drag
-        circle.remove();    // remove from 'svg'
-      }
-    );
+    // Handle back pressure by getting new entries only in response to our triggers.
+    //
+    //var triggerObs = new Rx.Subject();    //.startWith(true);
+
+    //triggerObs.onNext(true);
+
+    // tbd. What, there's no '.throttleFirst'? AKa090116
+    //
+    //console.log(dragObs.throttleFirst);
+
+    dragObs.sample(20)
+      .subscribe(
+        function (o) {
+          //console.log( "Dragging: "+ o.x + " "+ o.y );
+          circle.center(o.x, o.y).show();
+          //triggerObs.onNext(true);   // ready for the next value
+        },
+        null,   // error handling
+        function () {  // end of drag
+          circle.remove();    // remove from 'svg'
+        }
+      );
   });
 
 })();
