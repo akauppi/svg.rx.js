4dd19a0b0ad47f6bd06e73106d2d8f1a64885478
Asko Kauppi <asko@bmdesign.fi>
2016-01-09 21:38:30 +0200

    Updates

--- demo4.js
+++ demo4.js
@@ -20,19 +20,33 @@
 
   var c= -1;   // next CSS color to use
 
+  // tbd. Would really like to describe animations via CSS, not here in code. AKa090116
+  //      - initial "pop-up" from nothing (past the radius, then bouncing back)
+  //      - final reduction to nothing
+
   svg.rx_draggable().subscribe( function (dragObs) {
     c = (c+1) % COLORS;
 
     var circle = svg.circle(R).addClass("n"+c).hide();
+    var fresh = true;
 
     dragObs.subscribe(
       function (o) {
         //console.log( "Dragging: "+ o.x + " "+ o.y );
-        circle.center(o.x, o.y).show();
+        circle.center(o.x, o.y);
+
+        if (fresh) {
+          circle.show();
+        }
       },
       null,   // error handling
       function () {  // end of drag
-        circle.remove();    // remove from 'svg'
+        circle.remove();
+        /* not stylish
+        circle.animate(500 /_*ms*_/).attr( { r: 0 } ).after( function () {
+          this.remove();    // remove from SVG
+        } );
+        */
       }
     );
   });
--- src/svg.rx.js
+++ src/svg.rx.js
@@ -56,10 +56,12 @@
     //
     var doc = isDoc ? el : /*el.parent(SVG.Nested) ||*/ el.parent(SVG.Doc);
 
-    // tbd. Can we do these within 'svg.js', without using the '.node' (i.e. dropping to plain SVG APIs)?
+    // Note: svg.js handles points using '[Int, Int]'; it doesn't have an abstraction over the SVG native point
+    //      class. Also, it does not seem to provide a way to actually apply the 'SVG.Matrix' on a pair of points.
+    //      See -> https://github.com/wout/svg.js/issues/437
     //
-    var buf = doc.node.createSVGPoint();             // point buffer (allocated just once per drag)
-    var matrix = el.node.getScreenCTM().inverse();    // calculated just once per drag
+    var buf = doc.node.createSVGPoint();            // point buffer (allocated just once per drag)
+    var matrix = el.screenCTM().inverse();          // calculated just once per drag
 
     // Transform from screen to user coordinates
     //
@@ -70,7 +72,7 @@
       buf.x = o.clientX;  // - (offset || 0)
       buf.y = o.clientY;
 
-      return buf.matrixTransform(matrix);
+      return buf.matrixTransform( matrix.native() );
     };
 
     /** DISABLED text element support not needed, yet
