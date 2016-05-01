5ff203419191884bf463f44508d698091098b363
Asko Kauppi <asko@bmdesign.fi>
2016-05-01 11:44:08 +0300

    mocha-phantomjs set up (not fully testing SVG yet)

--- demo/debug.js
+++ demo/debug.js
@@ -10,13 +10,15 @@
   var assert = chai.assert;
 
   var SIDE = 30;
+  var T= Math.sqrt( SIDE*SIDE/2 );
+  console.log("T:",T);
+
+  var X = 200;
+  var Y = 100;
 
   var svg = SVG("cradle");
 
-  //var gx = svg.rx.gx( svg.rect(10,10) ).translate(5,5);
-  //gx.pos(200,100);
-
-  var rect= svg.rect(SIDE,SIDE).translate(-SIDE/2,-SIDE/2).move(200,100).rotate(45);
+  var rect= svg.rect(SIDE,SIDE).translate(-SIDE/2,-SIDE/2).move(X,Y).rotate(45);
 
   /***
   // '.tbox()' gives the bounding box, with transformations applied (ie. in screen coords):
@@ -58,6 +60,9 @@
   console.log( rect.native().getBoundingClientRect() );
   */
 
+  /***
+  * Poking works, but is a bit imprecice.
+  *
   // offset of the SVG (needs to be added to use '.elementFromPoint()'
   //
   var ox = svg.native().offsetLeft;
@@ -67,20 +72,33 @@
   assert( document.elementFromPoint(ox+200-SIDE/2, oy+100) === rect.native() );
   assert( document.elementFromPoint(ox+200-SIDE/2, oy+100+SIDE/2) !== rect.native() );
   assert( document.elementFromPoint(ox+200-SIDE/2, oy+100-SIDE/2) !== rect.native() );
+  ***/
 
-  // ...
+  var ox = svg.native().offsetLeft;
+  var oy = svg.native().offsetTop;
 
   var m= rect.screenCTM().inverse();
   console.log(m);
 
-  // (0,0) should be ...
-  // (10,10) should be ...
-  //
-  var t= SIDE* Math.sqrt(0.5);
+  var m2 = rect.matrixify();
+  console.log(m2);
 
-  //assert.closeTo( top, 100-t, 0.01 );
-  //assert.closeTo( bottom, 100+t, 0.01 );
-  //assert.closeTo( left, 200-t, 0.01 );
-  //assert.closeTo( right, 200+t, 0.01 );
+  var rbox = rect.rbox();
+  console.log("rbox:", rbox);
+
+  svg.rect(rbox.width, rbox.height).translate( rbox.x, rbox.y ).addClass("debug");
+  svg.plain("rbox").translate(X+2*T,Y).addClass("debug");
+
+  // For some reason, the '.y' reported by '.rbox()' is systematically 0.43750762939453125 too big.
+  //
+  var OFFSET_Y = 0.43750762939453125;
+
+  // (0,0) should be (ox+200, oy+100)
+  // (10,10) should be (ox+200+T, oy+100+T
+  //
+  assert.closeTo( rbox.x, X-T, 0.01 );
+  assert.closeTo( rbox.y-OFFSET_Y, Y-T, 0.01 );
+  assert.closeTo( rbox.width, 2*T, 0.01 );
+  assert.closeTo( rbox.height, 2*T, 0.01 );
 
 }());
--- test/NoTest.js
+++ test/NoTest.js
@@ -1,18 +1,43 @@
+/*global: SVG, assert, describe, it*/
+/*jshint devel: true */
 
-var assert = require('chai').assert;
+// References:
+//    'Should' style of Chai -> http://chaijs.com/guide/styles/
+//    Phantomjs, Mocha and Chai for functional testing -> http://doublenegative.com/phantomjs-mocha-and-chai-for-functional-testing/
 
-describe('SVG.Rx', function() {
-  describe('gx', function () {
-    it('should be positionable by (Num,Num)', function () {
+mocha.ui('bdd');
 
+var assert = chai.assert;
+chai.should();
+
+console.log(chai);
+
+describe('just testing', function () {    // sample on how to test positions
+
+  beforeEach(function () {
+    //paper.clear();
+  });
+  afterEach(function () {
+  });
+
+  it('rect\'s position should be testable', function () {
+
+    var SIDE = 10;
+    var T= Math.sqrt( SIDE*SIDE/2 );
+    var X= 100;
+    var Y= 50;
+
+    if (false) {
       var svg = SVG();
-      var gx = svg.rx.gx( svg.rect(-10,-10,10,10) );
+      var rect= svg.rect(SIDE,SIDE).translate(-SIDE/2,-SIDE/2).move(X,Y).rotate(45);
 
-      gx.pos(200,100);
+      var rbox = rect.rbox();
+      var OFFSET_Y = 0.43750762939453125;   // for some reason, 'rbox' y values are this much off (at least in browsers)
 
-      assert
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+      (rbox.x).should.be.closeTo( X-T, 0.01 );
+      (rbox.y-OFFSET_Y).should.be.closeTo( Y-T, 0.01 );
+      (rbox.width).should.be.closeTo(2*T, 0.01);
+      (rbox.height).should.be.closeTo(2*T, 0.01);
+    }
   });
 });
