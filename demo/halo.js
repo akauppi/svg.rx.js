bc29732cf458e2179fb2e6d13bccc02a79fc0692
Asko Kauppi <asko@bmdesign.fi>
2016-07-04 23:48:10 +0300

    Kind of got the use+symbol to work (but path still behaves way better)

--- demo/halo.js
+++ demo/halo.js
@@ -6,10 +6,14 @@
 
 var RAD2DEG = (180.0 / Math.PI);
 
+var svg = SVG( document.body );
+
 /*
-* Halo demo
+* Halo demo, not using symbols (works)
+*
+* Note: This method scales by the browser zooming changes (symbols + use don't).
 */
-(function() {
+if (true) (function() {
   "use strict";
 
   var R1 = 20,
@@ -17,69 +21,19 @@ var RAD2DEG = (180.0 / Math.PI);
     X= 100,
     Y= 55;
 
-  var svg = SVG( document.body );
-
-  /*
-  * Make a group with a rectangle and the symbol fitting inside it.
-  */
-  /*** disabled
-  (function () {
-    var g = svg.group();
-    g.rect(20,20).center(0,0).addClass("debug");
-
-    var use = g.use("icon-forward", "halo-icons.svg");
-    //use.scale(0.06, 0,0);
-
-    console.log(use.x(), use.y());
-
-    g.move(200,50);
-  })();
-  ***/
-
-  /*** Disabled: 'use' gave too big placement of the symbols, and they were not coming on top of menu arc, either,
-  *     like rect's do. Trying another approach.
-  *
-  // Refer to the symbols in an external SVG file
+  // Don't use 'use' and symbols. Just define the icons here.
   //
-  var use1= svg.use("icon-forward", "halo-icons.svg");
-  var use2= svg.use("icon-trash", "halo-icons.svg");
+  var el1 = svg.path("M16.711 8.29l-6-5.996c-0.391-0.391-1.026-0.391-1.417 0s-0.391 1.025 0 1.417l4.293 4.29h-11.59c-0.553 0-1.001 0.448-1.001 1s0.448 1 1.001 1h11.59l-4.292 4.29c-0.391 0.391-0.391 1.025 0.001 1.417s1.026 0.391 1.417 0l6-5.997c0.196-0.196 0.294-0.453 0.294-0.71s-0.097-0.514-0.294-0.71z");
 
-  var trans = function (el) {   // SVG.Use -> SVG.Use (same)
-    el.translate(9,9);    // make the center its (0,0); we know the viewbox is 18,18
-    el.scale(0.06);
-
-    //console.log( el.width(), el.height() );   // 0,0 ???
-
-    // Note: If we pass 'use' entities to the 'svg.gxHalo()' they won't be properly rotated (actually - translated).
-    //    Wrap in a group. AKa210616
+  var el2 = svg.group();
     //
-    var g= el.parent().group();
-    g.add(el);
-
-    // tbd. Why do the rect end up elsewhere than the symbols? AKa220616
-    g.rect(20,20).center(0,0).addClass("debug");
-
-    return g;
-  }
-  ***/
-
-  // Don't use 'use'. Just define the icons here. #temporary
-  //
-  var use1 = (function () {     // () -> SVG.Element
-    return svg.path("M16.711 8.29l-6-5.996c-0.391-0.391-1.026-0.391-1.417 0s-0.391 1.025 0 1.417l4.293 4.29h-11.59c-0.553 0-1.001 0.448-1.001 1s0.448 1 1.001 1h11.59l-4.292 4.29c-0.391 0.391-0.391 1.025 0.001 1.417s1.026 0.391 1.417 0l6-5.997c0.196-0.196 0.294-0.453 0.294-0.71s-0.097-0.514-0.294-0.71z");
-  })();
-
-  var use2 = svg.group();
-    //
-    use2.path( "M17 5h-4v-3c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v3h-4c-0.552 0-1 0.448-1 1v1h2v9c0 0.552 0.448 1 1 1h12c0.552 0 1-0.448 1-1v-9h2v-1c0-0.552-0.448-1-1-1zM7 3h4v2h-4v-2zM14 15h-10v-8h10v8z" );
-    use2.path( "M6.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
-    use2.path( "M10.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
-
-  // tbd. using 'use' as elems doesn't seem to work - why don't they move? AKa210616
+    el2.path( "M17 5h-4v-3c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v3h-4c-0.552 0-1 0.448-1 1v1h2v9c0 0.552 0.448 1 1 1h12c0.552 0 1-0.448 1-1v-9h2v-1c0-0.552-0.448-1-1-1zM7 3h4v2h-4v-2zM14 15h-10v-8h10v8z" );
+    el2.path( "M6.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
+    el2.path( "M10.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
 
   var halo = svg.gxHalo(R1, R2, [
-    {el: /*trans(use1)*/ use1, f: function () { console.log("1"); }},
-    {el: /*trans(use2)*/ use2, f: function () { console.log("2"); }},
+    {el: el1, f: function () { console.log("1"); }},
+    {el: el2, f: function () { console.log("2"); }},
     {el: svg.rect(18,18), f: function () { console.log("3"); }},
     {el: svg.rect(18,18).style( {fill: "blue" }), f: function () { console.log("4"); }},
     {el: svg.rect(18,18).style( {fill: "red" }), f: function () { console.log("5"); }}
@@ -87,12 +41,82 @@ var RAD2DEG = (180.0 / Math.PI);
 
   halo.pos(X,Y);
 
-  // Show one menu on the screen, to begin with. When clicked, it shall close.
+  // tbd. features by the clicks
   //
-  // Clicking on the canvas moves the menu there shows the menu, again.
-
   svg.rx_draggable().subscribe( function (dragObs) {
   } );
+
+})();
+
+/*
+* Halo demo, using 'use' and symbols (does not work - symbols end up in wrong places) AKa040716
+*/
+(function() {
+  "use strict";
+
+  var R1 = 20,
+    R2 = 50,
+    X= 230,
+    Y= 55;
+
+  /*
+  * Make a group with a rectangle and the symbol fitting inside it.
+  */
+  if (false) (function () {
+    var g = svg.group();
+    g.rect(20,20).center(0,0).addClass("debug");
+
+    var use = g.use("icon-forward", "halo-icons.svg");
+    //use.scale(0.06, 0,0);
+
+    g.move(200,50);
+  })();
+
+  /*
+  * Note: 'use' gives too big placement of the symbols, and they were not coming on top of menu arc, either, like
+  *     rect's do.
+  */
+  // Refer to the symbols in an external SVG file
+  //
+  var use1= svg.use("icon-forward", "halo-icons.svg");
+  var use2= svg.use("icon-trash", "halo-icons.svg");
+
+  var trans = function (el) {   // SVG.Use -> SVG.Use (same)
+
+    // Note: These adjustments just *happen* to bring the right outcome (on default zooming level, in Safari and Chrome).
+    //      We should make this more systematic, so any scaling and zooming would work. AKa040716
+    //
+    el.translate(-8,1);    // make the center its (0,0); we know the viewbox is 18,18
+    el.scale(0.04);
+
+    // Note: If we pass 'use' entities to the 'svg.gxHalo()' they won't be properly rotated (actually - translated).
+    //    Wrap in a group. AKa210616
+    //
+    if (true) {
+      var g= el.parent().group();
+      g.add(el);
+
+      if (true) {
+        g.rect(20,20).addClass("debug");
+      }
+
+      return g;
+    } else {
+      return el;
+    }
+  }
+
+  // tbd. using 'use' as elems doesn't seem to work - why don't they move? AKa210616
+
+  var halo = svg.gxHalo(R1, R2, [
+    {el: trans(use1), f: function () { console.log("1"); }},
+    {el: trans(use2), f: function () { console.log("2"); }},
+    {el: svg.rect(18,18), f: function () { console.log("3"); }},
+    {el: svg.rect(18,18).style( {fill: "blue" }), f: function () { console.log("4"); }},
+    {el: svg.rect(18,18).style( {fill: "red" }), f: function () { console.log("5"); }}
+  ]);
+
+  halo.pos(X,Y);
 
 })();
 
