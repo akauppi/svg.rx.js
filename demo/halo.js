1e2a58435a4c58a0f311170d1fe6d0f5da889164
Asko Kauppi <asko@bmdesign.fi>
2016-07-10 16:33:02 +0300

    interim; started making the menu with an opening (not full circle)

--- demo/halo.js
+++ demo/halo.js
@@ -17,23 +17,27 @@ if (true) (function() {
   "use strict";
 
   var R1 = 20,
-    R2 = 50,
+    R2 = 70,
     X= 100,
-    Y= 55;
+    Y= 75;
 
-  // Don't use 'use' and symbols. Just define the icons here.
-  //
-  var el1 = svg.path("M16.711 8.29l-6-5.996c-0.391-0.391-1.026-0.391-1.417 0s-0.391 1.025 0 1.417l4.293 4.29h-11.59c-0.553 0-1.001 0.448-1.001 1s0.448 1 1.001 1h11.59l-4.292 4.29c-0.391 0.391-0.391 1.025 0.001 1.417s1.026 0.391 1.417 0l6-5.997c0.196-0.196 0.294-0.453 0.294-0.71s-0.097-0.514-0.294-0.71z");
+  var arrowRight = svg.path("M16.711 8.29l-6-5.996c-0.391-0.391-1.026-0.391-1.417 0s-0.391 1.025 0 1.417l4.293 4.29h-11.59c-0.553 0-1.001 0.448-1.001 1s0.448 1 1.001 1h11.59l-4.292 4.29c-0.391 0.391-0.391 1.025 0.001 1.417s1.026 0.391 1.417 0l6-5.997c0.196-0.196 0.294-0.453 0.294-0.71s-0.097-0.514-0.294-0.71z");
 
-  var el2 = svg.group();
+  var trash = svg.group();
     //
-    el2.path( "M17 5h-4v-3c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v3h-4c-0.552 0-1 0.448-1 1v1h2v9c0 0.552 0.448 1 1 1h12c0.552 0 1-0.448 1-1v-9h2v-1c0-0.552-0.448-1-1-1zM7 3h4v2h-4v-2zM14 15h-10v-8h10v8z" );
-    el2.path( "M6.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
-    el2.path( "M10.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
+    trash.path( "M17 5h-4v-3c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v3h-4c-0.552 0-1 0.448-1 1v1h2v9c0 0.552 0.448 1 1 1h12c0.552 0 1-0.448 1-1v-9h2v-1c0-0.552-0.448-1-1-1zM7 3h4v2h-4v-2zM14 15h-10v-8h10v8z" );
+    trash.path( "M6.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
+    trash.path( "M10.5 13h1c0.276 0 0.5-0.224 0.5-0.5v-3c0-0.276-0.224-0.5-0.5-0.5h-1c-0.276 0-0.5 0.224-0.5 0.5v3c0 0.276 0.224 0.5 0.5 0.5z" );
 
-  var halo = svg.gxHalo(R1, R2, [
-    {el: el1, f: function () { console.log("1"); }, disabled: true},
-    {el: el2, f: function () { console.log("2"); }},
+  var letter = svg.path("M512 96h-448c-17.672 0-32 14.328-32 32v320c0 17.672 14.328 32 32 32h448c17.672 0 32-14.328 32-32v-320c0-17.672-14.328-32-32-32zM467.781 160l-179.781 122.602-179.781-122.602h359.562zM480 400c0 8.836-7.156 16-16 16h-352c-8.844 0-16-7.164-16-16v-171.602l175.906 119.141c4.969 2.977 10.532 4.461 16.094 4.461s11.125-1.484 16.094-4.461l175.906-119.141v171.602z")
+    .scale(16/512, 0,0)
+    .translate(-8,-8);
+
+  var widthDeg = 50;
+
+  var halo = svg.gxHalo(R1, R2, widthDeg, [
+    {el: arrowRight, f: function () { console.log("1"); }, disabled: true},
+    {el: trash, el2: letter, f: function () { console.log("2"); }},
     {el: svg.rect(18,18), f: function () { console.log("3"); }},
     {el: svg.circle(18,18).style( {fill: "blue" }), f: function () { console.log("4"); }},
     {el: svg.circle(18,18).style( {fill: "red" }), f: function () { console.log("5"); }}
@@ -41,16 +45,26 @@ if (true) (function() {
 
   halo.pos(X,Y);
 
-  // tbd. features by the clicks
+  // Check that icons remain upright even when rotated
   //
-  svg.rx_draggable().subscribe( function (dragObs) {
-  } );
+  //halo.rotDeg(20);
 
 })();
 
 /*
-* Halo demo, using 'use' and symbols (does not work - symbols end up in wrong places) AKa040716
+* Halo demo, using 'use' and symbols (fetched from external files)
+*
+* Note:
+*   It would be nice to be able to fetch icons from elsewhere. However, this approach (implementation) suffers from
+*   two problems:
+*
+*     - the icons don't scale along if the zooming factor in the browser is changed (they also change positions)
+*     - positioning and scaling the icons is "magical", just trying to find the right numbers to fit them in.
+*
+*   Those two are obviously the same problem. Anyways, it is a waste of time at this stage to try to fix them, since
+*   the "inlined" symbol definitions don't suffer from this. AKa100716
 */
+/* disabled
 (function() {
   "use strict";
 
@@ -59,9 +73,9 @@ if (true) (function() {
     X= 230,
     Y= 55;
 
-  /*
+  /_*
   * Make a group with a rectangle and the symbol fitting inside it.
-  */
+  *_/
   if (false) (function () {
     var g = svg.group();
     g.rect(20,20).center(0,0).addClass("debug");
@@ -72,10 +86,10 @@ if (true) (function() {
     g.move(200,50);
   })();
 
-  /*
+  /_*
   * Note: 'use' gives too big placement of the symbols, and they were not coming on top of menu arc, either, like
   *     rect's do.
-  */
+  *_/
   // Refer to the symbols in an external SVG file
   //
   var use1= svg.use("icon-forward", "halo-icons.svg");
@@ -122,4 +136,4 @@ if (true) (function() {
   halo.pos(X,Y);
 
 })();
-
+***/
--- src/gx-halo.js
+++ src/gx-halo.js
@@ -12,44 +12,60 @@
   assert(Gx);
 
   var RAD_TO_DEG = 180.0 / Math.PI;
+  var DEG_TO_RAD = 1/RAD_TO_DEG;
 
-  //--- GxHalo ---
+  //--- GxHalo -> Gx
   //
   // Use:
-  //    new GxHalo(parent, 20, 50, [el1, el2, ...])
+  //    new GxHalo(parent, 20, 50, [o1, o2, ...])
   //
-  // .xxx()        Methods tbd.
+  // 'r1':    Inner radius
+  // 'r2':    Outer radius
+  // 'spread_deg':  Spread of each menu item (optional)
+  //              If provided, one can make menus which are arc-like, instead of covering the whole circle (default).
   //
-  var GxHalo = function (parent, r1, r2, choices) {    // (SVG.Container, Number, Number, array of {el: SVG.Elem, f: () ->}) ->
+  // 'oN':
+  //    .el:  The initial (or only) element
+  //    .el2: The alternative element, swapped when clicked (optional)
+  //    .f:   The callback function
+  //            Note: setting a menu entry to '.selected' or not is intentionally left to the callback
+  //
+  // Members:
+  //    ._spread_rad: Number
+  //
+  // Methods:
+  //    ._rot:      Place a single menu item to its place
+  //    .rotDeg:    Override of the 'Gx' rotation, rotating also the menu items so they remain upright.
+  //
+  var GxHalo = function (parent, r1, r2, spread_deg, choices) {    // (SVG.Container, Number, Number, Number, array of {el: SVG.Elem, el2: [SVG.Elem], f: () ->}) ->
     var self= this;
 
     var g = parent.group();
 
-    var spread_rad = 2*Math.PI / choices.length;
+    spread_deg = spread_deg || (360 / choices.length);
 
-    var n= choices.length;
+    var spread_rad = DEG_TO_RAD * spread_deg;
 
     g.circle(2*r1).center(0,0).addClass("debug");
     g.circle(2*r2).center(0,0).addClass("debug");
 
-    var rMid = (r1+r2)/2;
+    var rMid = (r1+r2)/2.0;
 
-    // Make a group with path and the inner stuff given (caller will translate)
+    // Make a group with arc path and the inner stuff.
     //
     // Returns the subgroup, for rotation and assigning the click handling
     //
     // Note: All this could be within the 'forEach' scope, instead of separate function.
     //
-    var addArcWithElem = function (_g,x,i) {   // (SVG.G, {el: SVG.Element, f: () ->, disabled: Boolean|undefined}, Int, () ->) -> SVG.G
+    var addG = function (_g,x,i) {   // (SVG.G, {el: SVG.Element, el2: [SVG.Element], f: () ->, disabled: [Boolean]}, Int) -> SVG.G
       var el = x.el,
+        el2 = x.el2,
         f = x.f,
         disabled = x.disabled || false;
 
       var gg= _g.group();
 
-      var rad = -(Math.PI/2) - i/n * (2.0*Math.PI);   // counter-clockwise, starting from top
-
-      // Make the path at 0 rad and rotate to rightful place.
+      // Make the arc path at 0 rad.
       //
       var a = Math.cos(spread_rad/2),
         b = Math.sin(spread_rad/2);
@@ -59,75 +75,100 @@
         rx2= r2*a,
         ry2= r2*b;
 
-      var path = gg.path( "M"+rx2+","+(-ry2)+
+      var arc = gg.path( "M"+rx2+","+(-ry2)+
         "A"+r2+","+r2+",0,0,1,"+rx2+","+ry2+     // larger arc, clockwise
         "L"+rx1+","+ry1+
         "A"+r1+","+r1+",0,0,0,"+rx1+","+(-ry1)+  // smaller arc, counter-clockwise
         "z"
       );
 
-      path.addClass("arc");   // to be able to limit styling to us
+      arc.addClass("arc");   // to be able to limit styling to us
 
-      var arc= path.rotate( rad * RAD_TO_DEG, 0,0 );
-
-      // Calculate the position for the icon. We don't want to rotate it.
+      // Angle of the center
       //
-      var rMid = (r1+r2)/2.0;
+      var rad = (function () {    // scope
+        var ii= i - (choices.length)/2 +0.5;
+        return Math.PI + spread_rad * ii;
+      })();
 
+      arc.rotate( rad * RAD_TO_DEG, 0,0 );
+
+      // Calculate the position for the icon.
+      //
       var dx = rMid * Math.cos(rad);
       var dy = rMid * Math.sin(rad);
 
+      // tbd. Some of the menu items are groups. 'svg.js' handling of them (or SVG itself) is elaborate, since it does
+      //    not allow transformations on the groups, it seems. We'd like to a) point the origin for the groups, b) then
+      //    here position simply by using '.move'. To be worked on once we, hopefully, break free from 'svg.js'. AKa100716
+      //
       el.center(dx,dy);
 
+      if (el2) {
+        el2.center(dx,dy);
+      }
+
+      // Add the elements to the group
+      //
       gg.add(arc);
       gg.add(el);
 
+      if (el2) {
+        gg.add(el2.hide());
+      }
+
+      // Initial disabled state
+      //
       if (disabled) {
         gg.addClass("disabled");
       }
 
-      /*** remove?
-      if (true) {
-        // Can use '.move' since the element has been translated to have (0,0) as the attachement point to the halo.
-        //
-        gg.add(el);
-        el.center( 0, -(r1+r2)/2.0 );
-          //.rotate( (-rad)* RAD_TO_DEG, 0,0 );
-      } else {
-        // Just place the icon in the right position, without joining it in the group (this way, it does not need to
-        // be rotated
-        //
-        var c = Math.cos(rad/2),
-          d = Math.sin(rad/2),
-          dx = rMid * d,
-          dy = rMid * c;
+      // Click handler
+      //
+      gg.click( function (ev) {
 
-        el.center(dx,dy);
-      }
-      ***/
-
-      gg.click( function () {
-        //alert("clicked");
-
-        // If disabled, do nothing
-        //
         if (gg.hasClass("disabled")) {
+          // nothing
         } else {
-          // Currently just toggle (needs at least: toggle by changing background and/or icon), just click, disabled. tbd.
+          // If there are two icons, swap them
           //
-          gg.toggleClass("selected");
+          if (el2) {
+            if (el.visible()) {
+              el.hide();
+              el2.show();
+            } else {
+              el2.hide();
+              el.show();
+            }
+          }
+
+          // tbd. Should we do the call so that 'gg' is the 'this'. AKa100716
           f();
         }
+      });
+
+      // Prevent default browser action (such as selecting text in the background body)
+      //
+      // Note: Calling '.preventDefault' on the 'click' event is not sufficient.
+      //
+      gg.mousedown( function (ev) {
+        ev.preventDefault();
+      });
+
+      gg.touchstart( function (ev) {
+        ev.preventDefault();
       });
     }
 
     // Add sections, one per elem
     //
     choices.forEach( function (x,i) {
-      addArcWithElem(g,x,i);
+      addG(g,x,i);
     });
 
     Gx.call( this, parent, g, "gxHalo" );
+
+    this.rotDeg(0);   // place the arcs and menu items to their initial places
 
     this.origin(0,0);
   };
@@ -136,11 +177,31 @@
 
   // tbd. If there are methods, add them here.
 
+  // --- Methods ---
+
+  assert( Gx.prototype.rotDeg );    // check there is a method we are overriding
+
+  // Override Gx's 'rotDeg' so that the menu entries stay vertically aligned. Base (rad 0) is east.
+  //
+  // This can be used by client code to rotate, but also *needs* to be called once, after 'gxHalo' creation, to put
+  // the things in right places.
+  //
+  // Note: We don't want to use regular rotation for the icons, but rather make sure they remain upright. That *could*
+  //    be the default behaviour for 'use' and symbols, but we had other problems getting that to work. AKa100716
+  //
+  GxHalo.prototype.rotDeg = function (deg) {    // (Number) -> ()
+    var self= this;
+
+    //var gs = self.children();   // the groups that cover 'path' for arc and the menu icon
+
+    // tbd.
+  }
+
   // tbd. Where and how we want to use the menu is still open (from 'SVG.Doc' or any 'Gx'?). AKa190616
   //
   SVG.extend( SVG.Doc, {
-    gxHalo: function (r1, r2, choices) {  // (Number, Number, array of { el: SVG.Element, f: () -> }) -> GxHalo
-      return new GxHalo(this, r1, r2, choices);
+    gxHalo: function (r1, r2, widthDeg, choices) {  // (Number, Number, Number, array of { el: SVG.Element, el2: SVG.Element, f: () -> }) -> GxHalo
+      return new GxHalo(this, r1, r2, widthDeg, choices);
     }
   });
 
